153e133d6528223eef026a44e3e3b89a76140a60
CA3SAR99 <111635700+CA3SAR99@users.noreply.github.com>
2025-07-28 16:07:14 +0200

    Create hello_world.rs

---
+++ hello_world.rs
@@ -0,0 +1,16 @@
+// Rust Programming notes 1 // 
+
+fn main() // main function, always executes first like in C
+{
+  println!("Hello World"); // since the function is a "rust macro" we need to include a '!' before the (argument) 
+}
+
+
+// Rust is a compiled language, similar to C.
+// You need to compile your Rust code before running it.
+
+// To compile:
+// rustc hello_world.rs
+
+// To run the compiled executable:
+// ./hello_world   // (on Unix-like systems such as Linux or macOS)
